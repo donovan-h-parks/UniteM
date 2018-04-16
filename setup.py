fe5a21182a5420173a180cbb1ae3237e15d47c16
Donovan Parks <donovan.parks@gmail.com>
2018-04-16 16:35:14 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ if __name__ == '__main__':
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
           'Natural Language :: English',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
           'Topic :: Scientific/Engineering :: Bio-Informatics',
           ],
         install_requires=["svgwrite>=1.1.9"],
